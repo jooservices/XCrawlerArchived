7507dbc243054148a914c73216d6ca29cf6486a8
Viet Vu <jooservices@gmail.com>
2021-05-13 22:18:08 +0700

    ^ Public horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -33,9 +33,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return true;
         });
     }
 }
