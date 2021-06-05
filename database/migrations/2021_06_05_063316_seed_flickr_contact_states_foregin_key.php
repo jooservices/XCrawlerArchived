a97026ed9fbc58aea0ff557cd8fce81375ae6431
Viet Vu <jooservices@gmail.com>
2021-06-05 14:05:11 +0700

    ^ Hotfix migration

--- database/migrations/2021_06_05_063316_seed_flickr_contact_states_foregin_key.php
+++ database/migrations/2021_06_05_063316_seed_flickr_contact_states_foregin_key.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\FlickrContact;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class SeedFlickrContactStatesForeginKey extends Migration
@@ -13,6 +16,15 @@ class SeedFlickrContactStatesForeginKey extends Migration
      */
     public function up()
     {
+        $now = Carbon::now();
+        DB::table('states')->updateOrInsert([
+            'reference_code' => FlickrContact::STATE_MANUAL,
+        ], [
+            'entity' => FlickrContact::class,
+            'state' => 'manual',
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
         Schema::table('flickr_contacts', function (Blueprint $table) {
             $table->foreign('state_code')->references('reference_code')->on('states');
         });
